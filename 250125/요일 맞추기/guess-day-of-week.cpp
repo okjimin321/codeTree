4a21ed39f8329b89087b808d68e37d8eed7a8a98
Jimin Ok <okjimin321@gmail.com>
2025-01-25 16:39:15 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250125/요일 맞추기/guess-day-of-week.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int m1, d1, m2, d2;
+
+string dayOfWeak[7] = {"Mon", "Tue", "Wed","Thu", "Fri", "Sat", "Sun"};
+
+int daysOfMonth[12] = {31, 28, 31,30, 31,30,31,31,30,31,30,31};
+
+int main() {
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    int totalA = d1;
+    for(int i  = 0; i < m1 - 1;i++){
+        totalA += daysOfMonth[i];
+    }
+
+    int totalB = d2;
+    for(int i  = 0; i < m2 - 1;i++){
+        totalB += daysOfMonth[i];
+    }
+
+    cout << dayOfWeak[(totalB - totalA + 7) % 7];
+
+    return 0;
+}
