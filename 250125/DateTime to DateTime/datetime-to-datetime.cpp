8668c02939847f0a807bc0fe0d2ae13073be6427
Jimin Ok <okjimin321@gmail.com>
2025-01-25 16:27:44 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250125/DateTime to DateTime/datetime-to-datetime.cpp
+++ 250125/DateTime to DateTime/datetime-to-datetime.cpp
@@ -4,29 +4,37 @@ using namespace std;
 
 int a, b, c;
 
-int main() {
+// int main() {
+//     cin >> a >> b >> c;
+
+//     int totalMin = 0;
+//     // Write your code here!
+//     if(a < 11){
+//         totalMin = -1;
+//     }
+//     else if(a == 11){
+//         if(b < 11){
+//             totalMin = -1;
+//         }
+//         else if(c < 11){
+//             totalMin = -1;
+//         }
+//         else{
+//             totalMin = (a - 11) * 24 * 60 + (b - 11) * 60 + c - 11;
+//         }
+//     }
+//     else{
+//         totalMin = (a - 11) * 24 * 60 + (b - 11) * 60 + c - 11;
+//     }
+
+//     cout << totalMin << '\n';
+//     return 0;
+// }
+
+int main(){
     cin >> a >> b >> c;
 
-    int totalMin = 0;
-    // Write your code here!
-    if(a < 11){
-        totalMin = -1;
-    }
-    else if(a == 11){
-        if(b < 11){
-            totalMin = -1;
-        }
-        else if(c < 11){
-            totalMin = -1;
-        }
-        else{
-            totalMin = (a - 11) * 24 * 60 + (b - 11) * 60 + c - 11;
-        }
-    }
-    else{
-        totalMin = (a - 11) * 24 * 60 + (b - 11) * 60 + c - 11;
-    }
+    int diff = ((a * 24 * 60) + (b * 60) + c) - ((11 * 24 * 60) + (11 * 60) + 11);
 
-    cout << totalMin << '\n';
-    return 0;
+    cout << diff << '\n';
 }
