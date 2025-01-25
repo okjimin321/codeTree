4ce86d01e94e245ce5c176028c34d6b1c439a209
Jimin Ok <okjimin321@gmail.com>
2025-01-25 16:22:44 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250125/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    cin >> a >> b >> c;
+
+    int totalMin = 0;
+    // Write your code here!
+    if(a < 11){
+        totalMin = -1;
+    }
+    else if(a == 11){
+        if(b < 11){
+            totalMin = -1;
+        }
+        else if(c < 11){
+            totalMin = -1;
+        }
+        else{
+            totalMin = (a - 11) * 24 * 60 + (b - 11) * 60 + c;
+        }
+    }
+    else{
+        totalMin = (a - 11) * 24 * 60 + (b - 11) * 60 + c - 11;
+    }
+
+    cout << totalMin << '\n';
+    return 0;
+}
