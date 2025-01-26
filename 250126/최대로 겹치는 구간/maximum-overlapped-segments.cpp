087a3b11df5e978d7a2be969dae02938f942761f
Jimin Ok <okjimin321@gmail.com>
2025-01-26 18:50:45 +0900

    Title: 최대로 겹치는 구간, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250126/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 250126/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -7,18 +7,18 @@ int x1[100], x2[100];
 
 int main() {
     cin >> n;
-    int* arr = new int[100];
+    int* arr = new int[200];
 
     for (int i = 0; i < n; i++) {
         cin >> x1[i] >> x2[i];
-        for(int j = x1[i]; j < x2[i]; j++){
+        for(int j = x1[i] + 100; j < x2[i] + 100; j++){
             arr[j]++;
         }
     }
 
     
     int m = 0;
-    for(int i = 0; i < 100; i++){
+    for(int i = 0; i < 200; i++){
         //cout << arr[i] << " ";
         m = max(m, arr[i]);
     }
