c302da2328dfddb141c52c928ec90ac45114ce3e
Jimin Ok <okjimin321@gmail.com>
2025-01-26 18:14:55 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250126/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int m1, m2, d1, d2;
+string A;
+
+string dayOfWeek[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+int daysOfMonth[12] = {31, 29, 31, 30, 31, 30,31, 31, 30, 31, 30, 31};
+
+int main() {
+    cin >> m1 >> d1 >> m2 >> d2;
+    cin >> A;
+
+    int totalA = d1;
+    for(int i = 0; i < m1 - 1 ; i++){
+        totalA += daysOfMonth[i];
+    }
+
+    int totalB = d2;
+    for(int i = 0; i < m2 - 1 ; i++){
+        totalB += daysOfMonth[i];
+    }
+
+    int diff = totalB - totalA;
+    for(int i = 0; i < 7; i++){
+        if(dayOfWeek[i] == A){
+            break;
+        }
+        diff++;
+    }
+
+    cout << diff / 7 << '\n';
+
+    // Write your code here!
+
+    return 0;
+}
