fb1894de7b8f165eff966e92597fa9f55a9c0281
Jimin Ok <okjimin321@gmail.com>
2025-01-27 19:52:18 +0900

    Title: 흰검 칠하기, Time: 8ms, Memory: 2MB, Status: Wrong Answer - CodeTree

--- 250127/흰검 칠하기/painting-white-black.cpp
+++ 250127/흰검 칠하기/painting-white-black.cpp
@@ -14,59 +14,68 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> x[i] >> dir[i];
 
-        if(dir[i] == 'R'){
-            loc[i + 1] = loc[i] + x[i];
-        }
-        else if(dir[i] == 'L'){
+       
+        if(dir[i] == 'L'){
             loc[i + 1] = loc[i] - x[i];
+        }
+        else if(dir[i] == 'R'){
+            loc[i + 1] = loc[i] + x[i];
         }
     }
 
+    // for(int i = 0; i < 10; i++){
+    //     cout << loc[i] << " ";
+    // }
+    // cout << endl;
 
-    int* checked = new int[1000 * 100 * 2];
-    int* lastest = new int[1000 * 100 * 2];
+    int* checked = new int[200000];
+    int* lastest = new int[200000];
     //initialize to distinguish white and black
-    for(int i = 0; i < 1000 * 100 * 2; i++){
+    for(int i = 0; i < (1000 * 100 * 2); i++){
         checked[i] = 1;
         lastest[i] = 0;
     }
 
+
+    //checking section
     for(int i = 0; i < 1000; i++){
         int start = min(loc[i], loc[i + 1]);
         int end = max(loc[i], loc[i + 1]);
         
-        int counter = 1;
-        int last = 0;
-
         //black
         if(dir[i] == 'R'){
-            counter = 3;
-            last = 1;
+            for(int j = start; j < end; j++){
+                checked[j + 100000] = checked[j + 100000] * 3;
+                lastest[j + 100000] = 69; 
+            }
         }
 
         //white
         else if(dir[i] == 'L'){
-            counter = 2;
-            last = 2;
+            for(int j = start; j < end; j++){
+                checked[j + 1000 * 100] = checked[j + 1000 * 100] * 2;
+                lastest[j + 1000 * 100] = 74; 
+            }
         }
 
-        for(int j = start; j < end; j++){
-            checked[j + 1000 * 100] = checked[j + 1000 * 100] * counter;
-            lastest[j + 1000 * 100] = last; 
-        }
+      
         
     }
 
     int g , b , w;
     g = b = w = 0;
-    for(int i = 0; i < 1000 * 100 * 2; i++){
+    
+    for(int i = 0; i < 200000; i++){
         if(checked[i] % 36 == 0){
+            cout << "g " << i - 100000<< '\n';
             g++;
         }
-        else if(lastest[i] == 1){
+        else if(lastest[i] == 69){
+            cout << "b " << i - 100000<< '\n';
             b++;
         }
-        else if(lastest[i] == 2){
+        else if(lastest[i] == 74){
+            cout << "w " << i - 100000<< '\n';
             w++;
         }
     }
