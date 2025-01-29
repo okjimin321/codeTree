159a857aece34b697807ee47e8925dea220cf978
Jimin Ok <okjimin321@gmail.com>
2025-01-29 11:31:21 +0900

    Title: 좌우로 움직이는 로봇, Time: 47ms, Memory: 8MB, Status: Passed - CodeTree

--- 250129/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
+++ 250129/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -10,7 +10,6 @@ char d_b[50000];
 
 int main() {
     cin >> n >> m;
-
     //setting location_a
     int location_a[MAX + 1]{};
     int time_a = 1;
@@ -49,30 +48,44 @@ int main() {
         }
     }
 
+    int time_max = max(time_a, time_b);
+    if(time_max == time_a){
+        for(int i = time_b; i < time_max; i++){
+            location_b[i] = location_b[i - 1];
+        }
+    }
+    else if(time_max == time_b){
+        for(int i = time_a ; i < time_max; i++){
+            location_a[i] = location_a[i - 1];
+        }
+    }
+
     int ans = 0;
     int cur_a = 1;
     int cur_b = 1;
     while(true){
         //checking whether they meet the condition
+
         if(location_a[cur_a] == location_b[cur_b] && location_a[cur_a - 1] != location_b[cur_b - 1]){
             ans++;
-            //cout << cur_a << '\n';
+    
         }
+        
+
 
         //increasing a, b
-        if(cur_a <= time_a){
+        if(cur_a < time_a){
             cur_a++;
         }
-        if(cur_b <= time_b){
+        if(cur_b < time_b){
             cur_b++;
         }
 
         //base case
-        if(cur_a == time_a + 1 && cur_b == time_b + 1){
+        if(cur_a == time_a  && cur_b == time_b ){
             break;
         }
     }
-
 
     cout << ans << '\n';
     // Write your code here!
