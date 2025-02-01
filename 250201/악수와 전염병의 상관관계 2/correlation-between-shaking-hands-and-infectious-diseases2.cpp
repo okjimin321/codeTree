efb7f82b820f8f0f41a4e1b24245888314fa66f6
Jimin Ok <okjimin321@gmail.com>
2025-02-01 15:12:17 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250201/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
+++ 250201/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -1,110 +1,64 @@
 #include <iostream>
-
+#include<queue>
 using namespace std;
 
 int N, K, P, T;
-int t[250]{};
-int x[250]{};
-int y[250]{};
 
-class Developer{
-public:
-    int idx;
-    bool infected;
-    int infect_count;
+struct Event{
+    int time;
 
-    Developer(){
-        idx = -1;
-        infected = false;
-        infect_count = -1;
+    int x_idx;
+    int y_idx;
+
+    Event(int t, int x, int y){
+        time = t;
+        x_idx = x;
+        y_idx = y;
     }
 
-    Developer(int idx, int infect_count):
-        idx{idx}, infect_count{infect_count}, 
-        infected{false}{}
-
-    void setInfected(){
-        infected = true;
-    }    
-
-    void setInfect(){
-        if(infect_count > 0){
-            infect_count--;
-        }
-        else{
-            //cout << "Cannot infect more!!\n";
-        }
+    bool operator<(const Event& other) const{
+        return time > other.time;
     }
 };
-
 
 int main() {
     cin >> N >> K >> P >> T;
 
-    //setting developers
-    Developer developers[N]{};
-    for(int i = 0; i < N; i++){
-        developers[i] = Developer(i, K);
-    }
-    //setting outbreak
-    developers[P - 1].setInfected();
-    
-    //T cases
+    priority_queue<Event> q;
     for (int i = 0; i < T; i++) {
-        cin >> t[i] >> x[i] >> y[i];
+        int t;
+        int x, y;
+        cin >> t >> x >> y;
 
+        Event e(t,x,y);
+        q.push(e);
     }
 
-    int cases[T]{};
+
+    int* infect_count = new int[N + 1]{};
+    bool* developers = new bool[N + 1]{};
+    developers[P] = true;
 
     for(int i = 0; i < T; i++){
-        int s = t[0];
-        int s_idx = 0;
-        for(int j = 1; j < T; j++){
-            if(s > t[j]){
-                s = t[j];
-                s_idx = j;
+        int x = q.top().x_idx;
+        int y = q.top().y_idx;
+        q.pop();
+        if(developers[x] == true){
+            if(infect_count[x] < K){
+                developers[y] = true;
             }
+            infect_count[x]++;
         }
-        t[s_idx] = 999;
-        cases[i] = s_idx;
-    }
-
-    //check who is infected
-    for(int j = 0; j < T; j++){
-
-        int i = cases[j];
-        //no infor
-        if(t[i] == 0){
-            continue;
-        }
-
-        if(developers[x[i] - 1].infected == true){
-
-            //parter is uninfected
-            if(developers[y[i] - 1].infected == false){
-                if(developers[x[i] - 1].infect_count > 0)
-                    developers[y[i] - 1].setInfected();
+        if(developers[y] == true){
+            if(infect_count[y] < K){
+                developers[x] = true;
             }
-            else{
-                developers[y[i] - 1].setInfect();
-            }
-
-            developers[x[i] - 1].setInfect();
-        }
-
-        else{
-            if(developers[y[i] - 1].infected == true){
-                if(developers[y[i] - 1].infect_count > 0){
-                    developers[x[i] - 1].setInfected();
-                }
-                developers[y[i] - 1].setInfect();
-            }
+            infect_count[y]++;
         }
     }
 
-    for(int i = 0; i < N; i++){
-        cout << developers[i].infected;
+    for(int i = 1; i <= N; i++){
+        cout << developers[i];
     }
 
     return 0;
