335a6e9d704ff2f5bfdade7e13be17f8afcdb305
Jimin Ok <okjimin321@gmail.com>
2025-02-02 15:59:41 +0900

    Title: 격자 위의 편안한 상태, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

--- 250202/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
+++ 250202/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int N, M;
 
 bool inRange(int x, int y){
-    return (0 <= x && x < 2000) && (0 <= y && y < 2000);
+    return (0 <= x && x < 200) && (0 <= y && y < 200);
 }
 
 int dx[4] = {-1, 0, 1, 0};
@@ -14,20 +14,20 @@ int dy[4] = {0, 1, 0, -1};
 int main() {
     cin >> N >> M;
 
-    bool arr[2000][2000]{};
+    bool arr[200][200]{};
  
     while(M--){
         int x, y;
         cin >> x >> y;
 
         //marking
-        arr[x + 500][y + 500] = true;
+        arr[x + 99][y + 99] = true;
 
         //counting
         int comfort_num = 0;
         for(int i = 0; i < 4; i++){
-            int nx = x + dx[i] + 500;
-            int ny = y + dy[i] + 500;
+            int nx = x + dx[i] + 99;
+            int ny = y + dy[i] + 99;
 
             if(inRange(nx, ny) && arr[nx][ny] == true){
                 comfort_num++;
