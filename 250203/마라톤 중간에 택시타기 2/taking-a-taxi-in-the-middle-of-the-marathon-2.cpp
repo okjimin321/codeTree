207a9ac84cc782c256454f5f4b78c21930d372ef
Jimin Ok <okjimin321@gmail.com>
2025-02-03 21:20:22 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250203/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<climits>
+
+using namespace std;
+
+int n;
+int x[100];
+int y[100];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
+    }
+
+    // Write your code here!
+    int ans = INT_MAX;
+
+    for (int i = 1; i < n - 1; i++) {
+        int sum = 0;
+        
+        for (int j = 0; j < n - 1; j++) {
+            if (j == i) {
+                continue;
+            }
+
+            else if (j == i - 1) {
+                sum += abs(x[i + 1] - x[i - 1]) + abs(y[i + 1] - y[i - 1]);
+            }
+            else {
+                sum += abs(x[j + 1] - x[j]) + abs(y[j + 1] - y[j]);
+            }
+        }
+        ans = min(ans, sum);
+    }
+
+    cout << ans << endl;
+    return 0;
+}
