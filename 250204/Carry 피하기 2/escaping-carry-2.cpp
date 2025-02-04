3f95e5538de655935fa61363861032cc271d662d
Jimin Ok <okjimin321@gmail.com>
2025-02-04 12:11:39 +0900

    Title: Carry 피하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250204/Carry 피하기 2/escaping-carry-2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include<climits>
+using namespace std;
+
+int n;
+int arr[20];
+
+bool isValid(int x, int y, int z) {
+    int mul = 1;
+    while (mul <= x || mul <= y || mul <= z) {
+        int x_val = (x / mul) % 10;
+        int y_val = (y / mul) % 10;
+        int z_val = (z / mul) % 10;
+
+        if (x_val + y_val + z_val > 9) {
+            return false;
+        }
+
+        mul *= 10;
+    }
+
+    return true;
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Write your code here!
+    int max_sum = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if (isValid(arr[i], arr[j], arr[k])) {
+                    max_sum = max(max_sum, arr[i] + arr[j] + arr[k]);
+                }
+            }
+        }
+    }
+
+    cout << max_sum << '\n';
+    return 0;
+}
