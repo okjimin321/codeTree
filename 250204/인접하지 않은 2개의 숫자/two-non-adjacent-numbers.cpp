bff6f33015adf389f789f0fdbb28b246ae4b2cff
Jimin Ok <okjimin321@gmail.com>
2025-02-04 11:52:22 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250204/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.cpp
+++ 250204/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.cpp
@@ -1,36 +1,24 @@
 #include <iostream>
 #include<climits>
-#include<cmath>
 using namespace std;
 
 int n;
-int numbers[100];
+int a[100];
 
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
-        cin >> numbers[i];
+        cin >> a[i];
     }
 
     // Write your code here!
-
     int max_sum = INT_MIN;
     for(int i = 0; i < n; i++){
-        int sum = numbers[i];
-
-        int max_num = INT_MIN;
-        for(int j = 0; j < n; j++){
-            if(abs(j - i) != 1 && i != j){
-                max_num = max(max_num, numbers[j]);
-            }
+        for(int j = i + 2; j < n; j++){
+            max_sum = max(max_sum, a[i] + a[j]);
         }
-        sum += max_num;
-
-        max_sum = max(max_sum, sum);
     }
-
-
+    
     cout << max_sum << endl;
-
     return 0;
 }
