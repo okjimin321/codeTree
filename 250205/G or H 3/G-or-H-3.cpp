31a0cc7bece3a216e31d097c2e2101d08e4f49dc
Jimin Ok <okjimin321@gmail.com>
2025-02-05 21:31:10 +0900

    Title: G or H 3, Time: 35ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250205/G or H 3/G-or-H-3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include<climits>
+using namespace std;
+
+int N, K;
+
+int main() {
+    cin >> N >> K;
+
+    char* arr = new char[10000 + 1]{};
+    for (int i = 0; i < N; i++) {
+        int x;
+        char c;
+        cin >> x >> c;
+
+        arr[x] = c;
+    }
+
+    int max_sum = INT_MIN;
+    for(int i = 1; i <= 10000 - K + 1 ; i++){
+        int sum = 0;
+        for(int j = i; j <= i + K; j++){
+            if(arr[j] == 'G'){
+                sum++;
+            }
+            else if(arr[j] == 'H'){
+                sum += 2;
+            }
+
+        }   
+        max_sum = max(max_sum, sum);
+    }
+
+    cout << max_sum << endl;
+    delete[] arr;
+    return 0;
+}
